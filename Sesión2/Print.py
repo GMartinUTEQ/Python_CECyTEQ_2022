380ff8c5cf0548e8557f5bc7d3aec1a56646c14b
GMartin <gabosoft@gmail.com>
2022-09-08 13:54:42 -0500

    Sesion 1 Print

---
+++ Sesión2/Print.py
@@ -0,0 +1,14 @@
+# 1. Imprimir Hola Mundo
+#print("Hola Mundo")
+
+# 2. Imprimir Hola Mundo Separado
+#print("Hola", 'Mundo')
+
+# 3. Imprimir usando parámetros.
+#print("Hola", "Mundo", sep="*", end="<<fin>>")
+
+# 4. MAL Uso del Exec
+#exec = "print('Hola Mundo')"
+
+# 5. Buen uso del Exce
+#exec("print('Hola Mundo')")
